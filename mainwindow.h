75b3c62567774eb65144e64e47b46690693a7bc2
PowerOleg <geraskin.oleg@mail.ru>
2024-10-19 13:03:30 +0300

    Initial commit

---
+++ mainwindow.h
@@ -0,0 +1,31 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include "dialog.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_btn_newDialog_clicked();
+
+
+    void on_btn_ok_clicked();
+
+    void on_btn_cancel_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    Dialog* dialog;
+};
+#endif // MAINWINDOW_H
